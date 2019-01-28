4c9f95b0c2ae36ef17194300aa3a7aca93523fc0
dksmith328 <dksmith328@gmail.com>
2019-01-28 15:56:46 -0700

    Delete Themes and Plugins & LightBox

---
+++ wp-login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; dksmith  |  developer &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='https://dksmith328.github.io/portfolio/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='https://dksmith328.github.io/portfolio/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='https://dksmith328.github.io/portfolio/wp-content/plugins/easy-social-icons/js/cnss.js?ver=1.0'></script>
+<link rel='stylesheet' id='cnss_font_awesome_css-css'  href='https://dksmith328.github.io/portfolio/wp-content/plugins/easy-social-icons/css/font-awesome/css/font-awesome.min.css?ver=4.7' type='text/css' media='all' />
+<link rel='stylesheet' id='cnss_css-css'  href='https://dksmith328.github.io/portfolio/wp-content/plugins/easy-social-icons/css/cnss.css?ver=1.0' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://dksmith328.github.io/portfolio/wp-includes/css/dashicons.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://dksmith328.github.io/portfolio/wp-includes/css/buttons.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://dksmith328.github.io/portfolio/wp-admin/css/forms.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://dksmith328.github.io/portfolio/wp-admin/css/l10n.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://dksmith328.github.io/portfolio/wp-admin/css/login.min.css?ver=5.0.3' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+	<link rel="icon" href="https://dksmith328.github.io/portfolio/wp-content/uploads/2019/01/cropped-dks-icon-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://dksmith328.github.io/portfolio/wp-content/uploads/2019/01/cropped-dks-icon-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://dksmith328.github.io/portfolio/wp-content/uploads/2019/01/cropped-dks-icon-180x180.png" />
+<meta name="msapplication-TileImage" content="https://dksmith328.github.io/portfolio/wp-content/uploads/2019/01/cropped-dks-icon-270x270.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="https://dksmith328.github.io/portfolio/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="https://dksmith328.github.io/portfolio/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://dksmith328.github.io/portfolio/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://dksmith328.github.io/portfolio/">&larr; Back to dksmith  |  developer</a></p>
+		
+	</div>
+
+	
+	<script type='text/javascript' src='https://dksmith328.github.io/portfolio/wp-includes/js/jquery/ui/core.min.js?ver=1.11.4'></script>
+<script type='text/javascript' src='https://dksmith328.github.io/portfolio/wp-includes/js/jquery/ui/widget.min.js?ver=1.11.4'></script>
+<script type='text/javascript' src='https://dksmith328.github.io/portfolio/wp-includes/js/jquery/ui/mouse.min.js?ver=1.11.4'></script>
+<script type='text/javascript' src='https://dksmith328.github.io/portfolio/wp-includes/js/jquery/ui/sortable.min.js?ver=1.11.4'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
